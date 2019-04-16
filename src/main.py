7f2cd4b3ed19a3a2bc30e4ee2f536e39f0715318
acetylsv <acetylsv@gmail.com>
2019-04-16 16:42:16 +0800

    clean up

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import solver
-
-config = yaml.load(open('../config/config.yaml'))
-s = solver.PPR_Solver(config, None, 'train')
-while s.epoch < 1000:
-    s.train()
-    if s.epoch % 10 == 0:
-        s.eval()
